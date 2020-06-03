e98392954f20fe44581d863464f087a537640574
joaoMarinho94 <joaopaulim94@gmail.com>
2020-06-03 04:49:10 -0300

    Created routes and controllers for Items and Points

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
 
-routes.get('/users', (req, res) =>{
-  return res.status(200).json({ok: true});
-})
+routes.get('/items', ItemsController.index);
+
+routes.post('/points', PointsController.store);
+routes.get('/points/:id', PointsController.show);
+routes.get('/points', PointsController.index);
 
 export default routes;
